730b2dd51f215eae47f1ebded4c010152391665e
Yasith Samarakoon <samarakoonyasi21@gmail.com>
2025-12-28 01:00:03 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./Pages/Home"
+import AvailableCars from "./Pages/AvailableCars"
+import Login from "./Pages/Login"
+import SignUp from "./Pages/SignUp"
+
+
+
+function App() {
+  
+
+  return (
+  
+      <>
+      
+      </>
+
+   
+  )
+}
+
+export default App
